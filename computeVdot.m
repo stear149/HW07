9523d3662e61014d5d4c440d69673ac261baf520
Owen-Le-Coder <haber297@umn.edu>
2025-10-27 15:58:47 -0500

    Update computeVdot.m

--- computeVdot.m
+++ computeVdot.m
@@ -1,3 +1,36 @@
+%==========================================================================
+% function[dv] = computeVdot(t, V, ra, rb, La, Lb)
+%
+% Input Arguments:
+% t is the time since the beginning of the rainfall event. t is a scalar. 
+% t has units of [s]
+%
+% V is the volume of water in the ponds. V is a (2 × 1) matrix: 
+% V(1) is the volume in the first pond 
+% V(2) is the volume in the second pond. V has units of [m3]
+%
+% ra is the base radius of the first pond. ra is a scalar. 
+% ra has units of [m]
+%
+% rb is the base radius of the second pond. rb is a scalar. 
+% rb has units of [m]
+%
+% La is the length of the weir in the first pond. La is a scalar. 
+% La has units of [m]
+%
+% Lb is the length of the weir in the second pond. Lb is a scalar. 
+% Lb has units of [m]
+%
+% Returns:
+% dVdt is the first derivatives of the pond volumes with respect to time 
+% dVdt is a (2 × 1) matrix: 
+% dVdt(1) is the derivative for the first pond
+% dVdt(2) is the derivative for the second pond. dVdt has units of [m^3/s]
+%
+% Author: Group I
+%
+% Version 27 Oct. 2025
+%==========================================================================
 function [dv] = computeVdot(t, V, ra, rb, La, Lb)
     
     % --- Pond Alpha (Pond 1) ---
